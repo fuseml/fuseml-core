78dcd503e685a9a4d9df85fcdc6b17d0deb39be1
Stefan Nica <snica@suse.com>
2021-05-28 13:38:11 +0200

    CLI: Fixed handling of env variables for global options
    Due to a known limitation in the viper library [1], binding
    cobra flags to a go variable and to a viper environment
    variable at the same time is not supported.
    
    This commit implements a workaround [2] that updates the flags
    according to information extracted by viper from env variables
    and configuration files after the cobra logic has finished loading
    the flag variables.
    
    The commit also adds support for using an optional fuseml CLI
    configuration file where global configuration parameter values
    can be stored. This is a yaml file that can be located either at
    `$PWD/.fuseml/config.yaml` or at `$HOME/.fuseml/config.yaml`.
    
    The priority in which configuration options are loaded is:
    1. explicit set via command line args
    2. environment variable
    3. configuration file
    4. default flag value
    
    [1] https://github.com/spf13/viper/issues/671
    [2] https://carolynvanslyck.com/blog/2020/08/sting-of-the-viper/

--- pkg/cli/cmd.go
+++ pkg/cli/cmd.go
@@ -1,13 +1,16 @@
 package cli
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/fuseml/fuseml-core/pkg/cli/codeset"
 	"github.com/fuseml/fuseml-core/pkg/cli/common"
 	"github.com/fuseml/fuseml-core/pkg/cli/runnable"
 	"github.com/fuseml/fuseml-core/pkg/cli/workflow"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
@@ -19,19 +22,20 @@ func NewCmdRoot() *cobra.Command {
 		Use:   os.Args[0] + " [--url HOST | -s HOST] [--timeout SECONDS] [--verbose|-v]",
 		Short: "FuseML CLI",
 		Long:  "FuseML command line client",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			// You can bind cobra and viper in a few locations, but PersistencePreRunE on the root command works well
+			return initializeConfig(cmd)
+		},
 	}
 
 	pf := cmd.PersistentFlags()
-	pf.StringVarP(&o.URL, "url", "u", "http://localhost:8000", "URL where the FuseML service is running")
-	viper.BindPFlag("url", pf.Lookup("url"))
+	pf.StringVarP(&o.URL, "url", "u", common.DefaultFuseMLURL, "URL where the FuseML service is running")
 	viper.BindEnv("url", "FUSEML_SERVER_URL")
 
-	pf.IntVar(&o.Timeout, "timeout", 30, "maximum number of seconds to wait for response")
-	viper.BindPFlag("timeout", pf.Lookup("timeout"))
+	pf.IntVar(&o.Timeout, "timeout", common.DefaultHTTPTimeout, "maximum number of seconds to wait for response")
 	viper.BindEnv("timeout", "FUSEML_HTTP_TIMEOUT")
 
 	pf.BoolVarP(&o.Verbose, "verbose", "v", false, "print verbose information, such as HTTP request and response details")
-	viper.BindPFlag("verbose", pf.Lookup("verbose"))
 	viper.BindEnv("verbose")
 
 	cmd.AddCommand(codeset.NewCmdCodeset(o))
@@ -39,6 +43,53 @@ func NewCmdRoot() *cobra.Command {
 	cmd.AddCommand(workflow.NewCmdWorkflow(o))
 
 	return cmd
+}
+
+func initializeConfig(cmd *cobra.Command) error {
+
+	// Set the base name of the config file, without the file extension.
+	viper.SetConfigName(common.ConfigFileName)
+
+	// Set the config format and extension
+	viper.SetConfigType("yaml")
+
+	// Set paths where viper should look for the config file.
+	if dirname, err := os.Getwd(); err == nil {
+		viper.AddConfigPath(filepath.Join(dirname, common.ConfigHomeSubdir))
+	}
+	if dirname, err := os.UserHomeDir(); err == nil {
+		viper.AddConfigPath(filepath.Join(dirname, common.ConfigHomeSubdir))
+	}
+
+	// Attempt to read the config file, gracefully ignoring errors
+	// caused by a config file not being found. Return an error
+	// if we cannot parse the config file.
+	if err := viper.ReadInConfig(); err != nil {
+		// It's okay if there isn't a config file
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	// Bind the current command's flags to viper
+	bindFlags(cmd)
+
+	return nil
+}
+
+// Bind each cobra flag to its associated viper configuration (config file and environment variable)
+// This is required because viper doesn't work with cobra flags that are also bound to a variable
+// (e.g. using StringVar to bind a flag to a string variable). See https://github.com/spf13/viper/issues/671.
+func bindFlags(cmd *cobra.Command) {
+
+	cmd.Flags().VisitAll(func(f *pflag.Flag) {
+		// Apply the viper config value to the flag when the flag is not set and viper has a value
+		if !f.Changed && viper.IsSet(f.Name) {
+			val := viper.Get(f.Name)
+			cmd.Flags().Set(f.Name, fmt.Sprintf("%v", val))
+		}
+	})
+
 }
 
 // Execute creates the root cobra command, which in turn creates all sub-commands and sets all flags appropriately.
--- pkg/cli/common/global.go
+++ pkg/cli/common/global.go
@@ -3,6 +3,14 @@ package common
 const (
 	// Version is the CLI version
 	Version = "0.1"
+	// ConfigFileName is the name of the FuseML configuration file (without extension)
+	ConfigFileName = "config"
+	// ConfigHomeSubdir is the subdirectory where the FuseML configuration files is located
+	ConfigHomeSubdir = ".fuseml"
+	// DefaultFuseMLURL is the default URL to use for the FuseML server
+	DefaultFuseMLURL = "http://localhost:8000"
+	// DefaultHTTPTimeout is the default HTTP timeout value
+	DefaultHTTPTimeout = 30
 )
 
 // GlobalOptions contains global CLI configuration parameters
